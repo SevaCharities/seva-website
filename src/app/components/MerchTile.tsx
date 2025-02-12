e714af32cc8827923d220ab1af43a60d6ea0138b
Shiva <ShivaBala2015@gmail.com>
2025-02-12 17:52:20 -0600

    merch updates

---
+++ src/app/components/MerchTile.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState } from "react";
+import { Merch } from "../merch/page";
+import Image from "next/image";
+
+export const MerchTile = ({ m }: { m: Merch }) => {
+  const [isFront, setIsFront] = useState(true);
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center p-4 bg-white rounded-lg"
+      onMouseEnter={() => setIsFront(false)}
+      onMouseLeave={() => setIsFront(true)}
+    >
+      <div className="relative w-80 h-72">
+        <Image
+          src={m.image[0]}
+          alt={m.name}
+          fill
+          className={` object-contain rounded-lg transition-opacity duration-300 ${
+            isFront ? "opacity-100" : "opacity-0"
+          }`}
+        />
+        <Image
+          src={m.image.length > 1 ? m.image[1] : m.image[0]}
+          alt={m.name}
+          fill
+          className={`object-contain rounded-lg transition-opacity duration-300 absolute top-0 left-0 ${
+            isFront ? "opacity-0" : "opacity-100"
+          }`}
+        />
+      </div>
+      <div
+        className={`w-full flex items-center py-8 px-6 mt-4 rounded-lg ${
+          isFront ? "bg-orange-2" : "bg-green-500"
+        }`}
+      >
+        <h3 className="w-2/3 flex-wrap text-xl text-orange-0 font-sem">
+          {m.name}
+        </h3>
+        <p className="w-1/3 text-orange-0 text-3xl font-black text-right">
+          ${m.price}
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/app/merch/page.tsx
+++ src/app/merch/page.tsx
@@ -1,47 +1,52 @@
 import Image from "next/image";
+import { MerchTile } from "../components/MerchTile";
 
-interface Merch {
+export interface Merch {
+  id: number;
   name: string;
   price: number;
-  image: string;
+  image: string[];
 }
 
 const merch: Merch[] = [
-  { name: "Seva T-shirt (Home)", price: 7, image: "/tshirt.avif" },
-  { name: "Seva T-shirt (Away)", price: 8, image: "/tshirt.avif" },
-  { name: "Seva T-shirt (Away)", price: 8, image: "/tshirt.avif" },
+  {
+    id: 1,
+    name: "Seva T-shirt 2023 (Beige)",
+    price: 7,
+    image: ["/merch/seva1_front.png", "/merch/seva1_back.png"],
+  },
+  {
+    id: 2,
+    name: "Seva T-shirt 2022 (Blue)",
+    price: 8,
+    image: ["/merch/seva2_front.png"],
+  },
 ];
-
-const MerchTile = ({ m }: { m: Merch }) => {
-  return (
-    <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg ">
-      <div className="relative w-80 h-72">
-        <Image
-          src={m.image}
-          alt={m.name}
-          fill
-          className="object-cover rounded-lg"
-        />
-      </div>
-      <div className="w-full flex justify-between items-center py-8 px-6">
-        <h3 className="text-xl">{m.name}</h3>
-        <p className=" text-orange-2 text-lg  font-semibold">${m.price}</p>
-      </div>
-    </div>
-  );
-};
 
 const Page = () => {
   return (
     <div className="my-16 sm:my-24 flex flew-wrap flex-col justify-center items-center">
-      <h1 className="px-8 text-orange-2 font-semibold my-12 text-center ">
-        Merch
-      </h1>
+      <div className="my-12">
+        <h1 className="px-8 text-orange-2 font-semibold  text-center ">
+          Merch
+        </h1>
+        <p>
+          Interested in buying? Fill out this{" "}
+          <a
+            className=" bg-orange-2 text-white underline p-1"
+            href="https://www.example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google Form!
+          </a>
+        </p>
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {/* {merch.map((m: Merch, index: number) => (
+        {merch.map((m: Merch, index: number) => (
           <MerchTile key={index} m={m} />
-        ))} */}
+        ))}
       </div>
     </div>
   );
