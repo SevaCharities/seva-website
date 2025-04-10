0f830e7de35319384a607203719d5fcb366d8c8c
nikenpatel29 <nikenpatel1229@gmail.com>
2025-04-09 20:41:54 -0500

    Updated GM

--- src/app/general-meetings/page.tsx
+++ src/app/general-meetings/page.tsx
@@ -9,11 +9,30 @@ export type GMProps = {
 
 const GMInfo: GMProps[] = [
   {
-    meeting: 11,
+    meeting: 14,
     //slides:
-      //"https://docs.google.com/presentation/d/1qZ3UvwahjH4d6cwrlEGbUXGk6urwKK8VQy4pYwJRIzA/edit?usp=sharing",
-    date: ["Mon, Mar 10", "3/10/25"],
+      //"https://docs.google.com/presentation/d/1JYtHfXvyl96ncV5-cOv8LPc7Fj8imBA44N5gGVDphzM/edit?usp=sharing",
+    date: ["Mon, Apr 21", "4/21/25"],
     upcoming: true,
+  },
+  {
+    meeting: 13,
+    slides:
+      "https://docs.google.com/presentation/d/1JYtHfXvyl96ncV5-cOv8LPc7Fj8imBA44N5gGVDphzM/edit?usp=sharing",
+    date: ["Mon, Apr 7", "4/7/25"],
+    upcoming: true,
+  },
+  {
+    meeting: 12,
+    slides:
+      "https://file.groupme.com/v1/102302724/files/e9991882-1e36-4648-9a81-d5cf913b4363?access_token=RfAUZzHghZSJfRX1yoknHhBpcMNu44OKovGZ0CC4&omit-content-disposition=true",
+    date: ["Mon, Mar 24", "3/24/25"],
+  },
+  {
+    meeting: 11,
+    slides:
+      "https://file.groupme.com/v1/102302724/files/9e95836f-b35b-48d6-8453-706be46795d8?access_token=RfAUZzHghZSJfRX1yoknHhBpcMNu44OKovGZ0CC4&omit-content-disposition=true",
+    date: ["Mon, Mar 10", "3/10/25"],
   },
   {
     meeting: 10,
