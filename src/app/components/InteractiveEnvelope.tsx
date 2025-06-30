a908394c186bba02e66a9ddf8893547e8fff38aa
nikenpatel29 <nikenpatel1229@gmail.com>
2025-06-29 19:58:27 -0500

    Updated upcoming page

--- src/app/components/InteractiveEnvelope.tsx
+++ src/app/components/InteractiveEnvelope.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import { useState } from "react";
-import { UpcomingEventsProps } from "../upcoming-events/page";
+import { UpcomingEventsProps } from "../upcoming/page";
 
 const UpcomingEventsColors = [
   "",
