7585f5113910afebc20cbb4c2cfef38732785739
VR <145240391+visvesh-r48@users.noreply.github.com>
2026-03-07 16:56:47 -0600

    Modified upcoming events page

--- src/app/upcoming/page.tsx
+++ src/app/upcoming/page.tsx
@@ -11,23 +11,9 @@ export type UpcomingEventsProps = {
 
 const UpcomingEventsInfo: UpcomingEventsProps[] = [
   {
-    title: "Volleyball Social",
-    category: 1,
-    date: ["Wed, Mar 4", "Pease Park Volleyball Courts", "5:30 P.M. - 7 P.M."],
-    description: "Join us for a fun volleyball social at Pease Park! Whether you're a pro or just want to have fun, come out and play with us. All skill levels welcome!",
-    link: ["https://docs.google.com/forms/d/1XZoV2fY0J3uPo6N5Qaa9pON06k2sqk3gMr9KS2kvRK4/edit", true]
-  },
-  {
-    title: "It's My Park Day Phil",
-    category: 2,
-    date: ["Sat, Mar 7", "N Austin Community Garden/YMCA Pedestrian Walkway", "9 A.M. - 12 P.M."],
-    description: "Join us for the My Park Day Phil! We'll be helping to clean up the park and make it a better place for everyone. It's a great way to give back to the community and enjoy the outdoors!",
-    link: ["", false]
-  },
-  {
     title: "Seva Sips Pop-Up",
-    category: 3,
-    date: ["Sunday, Mar 8","Location Coming Soon", "10 A.M. - 2 P.M."],
+    category: 1,
+    date: ["Sunday, Mar 8","The Mark Austin", "10 A.M. - 2 P.M."],
     description: "Join us for our Seva Sips Pop-Up! We'll be selling delicious food and drinks to raise money for Akshaya Patra. Bring your friends and enjoy some tasty treats while supporting a great cause!",
     link: ["https://partiful.com/e/Q1tWijpbJP6pQlFurV95", true]
   }
